4bb32d285e5fc2ce27ec74c7158cfc5f37f16f26
zmy <573093826@qq.com>
2018-10-09 23:04:04 +0800

    init

---
+++ chat-back/src/main/java/org/peter/chatback/ChatBackApplication.java
@@ -0,0 +1,12 @@
+package org.peter.chatback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatBackApplication.class, args);
+    }
+}
