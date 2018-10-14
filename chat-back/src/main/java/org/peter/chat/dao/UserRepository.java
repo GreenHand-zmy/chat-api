84577736ca11f2aef646af2cae983d52e51931b3
zmy <573093826@qq.com>
2018-10-14 16:10:49 +0800

    移除jpa

--- chat-back/src/main/java/org/peter/chat/dao/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package org.peter.chat.dao;
-
-import org.peter.chat.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
