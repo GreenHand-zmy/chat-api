d65b69b604b441c390391caec48da78054f2b6a9
zmy <573093826@qq.com>
2018-10-15 21:40:21 +0800

    修改test

--- chat-back/src/test/java/org/peter/chat/mapper/UserMapperTest.java
+++ chat-back/src/test/java/org/peter/chat/mapper/UserMapperTest.java
@@ -1,5 +1,9 @@
 package org.peter.chat.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.interfaces.Compare;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.peter.chat.entity.User;
@@ -16,8 +20,19 @@ public class UserMapperTest {
     private UserMapper userMapper;
 
     @Test
-    public void testSelect() {
+    public void testSelectAll() {
         List<User> userList = userMapper.selectList(null);
         userList.forEach(System.out::println);
+    }
+
+    @Test
+    public void testSelectById() {
+        User user = userMapper.selectById("1");
+        System.out.println(user);
+    }
+
+    @Test
+    public void testQuery() {
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
     }
 }
