71370a8101696b53aad723e60ab203fc6d89d172
zmy <zhangmy_sx@taofen8.com>
2018-11-07 13:46:54 +0800

    修改pom文件

--- chat-back/src/main/java/org/peter/chat/controller/UserController.java
+++ chat-back/src/main/java/org/peter/chat/controller/UserController.java
@@ -1,5 +1,7 @@
 package org.peter.chat.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.peter.chat.domain.vo.UserVo;
 import org.peter.chat.entity.User;
 import org.peter.chat.service.UserService;
@@ -15,10 +17,12 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/user")
+@Api("用户控制器")
 public class UserController {
     @Autowired
     private UserService userService;
 
+    @ApiOperation(value = "用户登录或注册")
     @PostMapping("/registerOrLogin")
     ResultBean<UserVo> registerOrLogin(@Valid User user) {
         // 查询用户名是否已经存在
